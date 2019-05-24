6a0a7617c05d8bd8d431e121d888950adae7b51f
chloe.lepez@shayp.com <chloelepez@gmail.com>
2019-05-24 18:58:49 +0200

    with operations

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import Button from './components/Button';
 import Input from './components/Input';
 import Equal from './components/Equal';
-import * as math from 'mathjs';
 import './App.css';
 
 class App extends Component {
@@ -11,6 +10,9 @@ class App extends Component {
 
      this.state = {
        input: "",
+       operator: "",
+       n1: "",
+       n2: "",
      }
     }   
 
@@ -34,9 +36,43 @@ class App extends Component {
     this.setState({ input: ""})
   } 
 
+  add = () => {
+    this.setState({ n1: this.state.input });
+    this.setState({ input: "" });
+    this.setState({ operator: "add" });
+  };
+
+  substract = () => {
+    this.setState({ n1: this.state.input });
+    this.setState({ input: "" });
+    this.setState({ operator: "substract" });
+  };
+  
+  multiply = () => {
+    this.setState({ n1: this.state.input });
+    this.setState({ input: "" });
+    this.setState({ operator: "multiply" });
+  };
+
+  divide = () => {
+    this.setState({ n1: this.state.input });
+    this.setState({ input: "" });
+    this.setState({ operator: "divide" });
+  };
+
   calculate = () => {
-    this.setState({ input: math.eval(this.state.input)})
-  }
+    this.setState({ n2: this.state.input });
+
+    if (this.state.operator === "add") {
+      this.setState({ input: parseFloat(this.state.n1) + parseFloat(this.state.n2) });
+    } else if (this.state.operator === "substract") {
+      this.setState({ input: parseFloat(this.state.n1) - parseFloat(this.state.n2) });
+    } else if (this.state.operator === "multiply") {
+      this.setState({ input: parseFloat(this.state.n1) * parseFloat(this.state.n2) });
+    } else if (this.state.operator === "divide") {
+      this.setState({ input: parseFloat(this.state.n1) / parseFloat(this.state.n2) });
+    }
+  };
 
   render() {
     return (
@@ -49,25 +85,25 @@ class App extends Component {
             <Button handleClick={this.addToInput}>7</Button>
             <Button handleClick={this.addToInput}>8</Button>
             <Button handleClick={this.addToInput}>9</Button>
-            <Button handleClick={this.addToInput}>/</Button>
+            <Button handleClick={this.divide}>/</Button>
           </div>
           <div className="row">
             <Button handleClick={this.addToInput}>4</Button>
             <Button handleClick={this.addToInput}>5</Button>
             <Button handleClick={this.addToInput}>6</Button>
-            <Button handleClick={this.addToInput}>*</Button>
+            <Button handleClick={this.multiply}>*</Button>
           </div>
           <div className="row">
             <Button handleClick={this.addToInput}>1</Button>
             <Button handleClick={this.addToInput}>2</Button>
             <Button handleClick={this.addToInput}>3</Button>
-            <Button handleClick={this.addToInput}>+</Button>
+            <Button handleClick={this.add}>+</Button>
           </div>
           <div className="row">
             <Button handleClick={this.addDecimal}>,</Button>
             <Button handleClick={this.addZeroToInput}>0</Button>
             <Button handleClick={this.clearInput}>C</Button>
-            <Button handleClick={this.addToInput}>-</Button>
+            <Button handleClick={this.substract}>-</Button>
           </div>
           <div className="row">
           <Equal handleClick={this.calculate} />
